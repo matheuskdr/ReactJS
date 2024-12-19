5c8d2a69564be9d5727daf81eea4a3c52b8c4adb
matheuskdr <matheusgabriellkpo@gmail.com>
2024-12-18 22:18:22 -0300

    +6

--- aulas/aula04/src/app/page.tsx
+++ aulas/aula04/src/app/page.tsx
@@ -1,25 +1,25 @@
-import { Person } from "@/components/Person";
-
+import { peopleList } from '@/data/peopleList'
 
 const Page = () => {
-  return ( <div>
+
+  const chemists = peopleList.filter(person => person.profession === 'chemist');
+
+  return (
+    <div>
       <h1 className="font-bold text-2xl">Olá Mundo!</h1>
       <h3>Algum outro texto</h3>
 
-    <Person 
-      name="Elon Musk"
-      avatar="https://classic.exame.com/wp-content/uploads/2021/04/Elon-Musk.jpg"
-      roles={['CEO da Tesla', 'CEO da SpaceX']}
-    />
-    <Person 
-      name="Jeff Bezos"
-      avatar="https://melhorinvestimento.net/wp-content/uploads/2024/04/jeff-bezos-a-historia-do-homem-mais-rico-do-mundo-1-scaled-1.jpg"
-      roles={['CEO da Amazon', 'CEO da Blue Origin']}
-    />
-    <Person
-      name="João"
-      roles={['Algo', 'Dono de algo']}
-    />
+      {chemists.length > 0 &&
+        <>
+          <h3>Lista de químicos:</h3>
+          <ul>
+            {chemists.map(person =>
+              <li key={person.id}>{person.name}</li>
+            )}
+          </ul>
+        </>
+      }
+
     </div>
   );
 }
