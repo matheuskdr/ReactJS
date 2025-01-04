a5869e7b878bf032dc50a4b3136e28cd38dc3829
matheuskdr <matheusgabriellkpo@gmail.com>
2025-01-04 20:30:08 -0300

    +14

---
+++ exercicios/exc06/src/data/questions.ts
@@ -0,0 +1,24 @@
+import { Question } from "@/types/Question";
+
+export const questions: Question[] = [
+    {
+        question: 'Qual ingrediente não pode faltar?',
+        options: [
+            'Farinha',
+            'Corante',
+            'Agua',
+            'Açucar'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual o melhor método de cozinhar?',
+        options: [
+            'Microondas',
+            'Fogão',
+            'Fogueira',
+            'Lança-chamas'
+        ],
+        answer: 3
+    }
+];
