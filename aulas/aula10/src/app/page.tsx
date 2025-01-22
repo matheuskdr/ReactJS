75f1cbee6479d5b011da9cde53b74e19eb70d07a
matheuskdr <matheusgabriellkpo@gmail.com>
2025-01-21 21:18:49 -0300

    About API 2

--- aulas/aula10/src/app/page.tsx
+++ aulas/aula10/src/app/page.tsx
@@ -1,21 +1,61 @@
 "use client"
 
-import { useEffect } from "react";
+import { User } from "@/types/User";
+import { useEffect, useState } from "react";
 
 const Page = () => {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState<User[]>([]);
+
+  const getUsers = async () => {
+    setLoading(true);
+
+    try {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users');
+      const json = await res.json();
+      setUsers(json);
+    } catch (err) {
+      console.log('Deu tudo errado!')
+    }
+    setLoading(false);
+  }
 
   useEffect(() => {
-
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(json => {
-        console.log(json[0]);
-      })
+      getUsers();
   }, []);
+
+  const handleAddNewPost = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      },
+      body: JSON.stringify({
+        title: 'Post de teste',
+        body: 'Corpo de teste',
+        userId: 99
+      })
+    });
+    const json = await res.json();
+
+    console.log(json);
+  }
 
   return (
     <div className="container mx-auto">
+      <button onClick={handleAddNewPost}>Adicionar Novo Post</button>
+
       <h1 className="text-3xl">Lista de usuários</h1>
+
+      {loading && 'Carregando...'}
+      {!loading && users.length > 0 && 
+        <ul>
+          {users.map(item => (
+            <li key={item.id}>{item.name} - ({item.address.city})</li>
+          ))}
+        </ul>
+      }
+      {!loading && users.length === 0 && 'Não há usuários para exibir.'}
     </div>
   );
 }
