932ec62b412e9d3730fe4ee7397783f872b33c54
matheuskdr <matheusgabriellkpo@gmail.com>
2025-02-06 22:23:39 -0300

    About shadcn

---
+++ aulas/aula15/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Button >Clique aqui</Button>
+    </div>
+  );
+}
+
+export default Page;
