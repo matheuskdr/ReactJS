218ead171eb09fa1c2f95c12be3cd86d55363e63
matheuskdr <matheusgabriellkpo@gmail.com>
2025-02-11 21:53:18 -0300

    About shadcn

--- aulas/aula15/src/app/page.tsx
+++ aulas/aula15/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
 
 const Page = () => {
   return (
     <div className="min-h-screen flex justify-center items-center">
       <Button >Clique aqui</Button>
+      <ThemeToggle />
     </div>
   );
 }
