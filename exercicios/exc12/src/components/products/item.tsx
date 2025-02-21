b405c5e38ec68ed515df58edb500e9d47d6b4998
matheuskdr <matheusgabriellkpo@gmail.com>
2025-02-20 22:42:50 -0300

    about exc12

---
+++ exercicios/exc12/src/components/products/item.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Product } from "@/types/product";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+import { useCartStore } from "@/stores/cart-store";
+
+type Props = {
+    item: Product;
+}
+
+export const ProductItem = ({ item }: Props) => {
+    const { toast } = useToast();
+    const { upsertCartItem } = useCartStore(state => state);
+
+
+    const handleAddButton = () => {
+        upsertCartItem(item, 1);
+        toast({
+            title: "Produto adicionado",
+            description: `${item.name} foi adicionado ao carrinho.`,
+            action: <ToastAction altText="fechar">Fechar</ToastAction> 
+        });
+    }
+
+    return (
+        <div>
+            <div className="rounded-md overflow-hidden">
+                <img
+                    src={item.image}
+                    alt={item.name}
+                    className="w-full h-32 object-cover"
+                />
+            </div>
+            <div className="mt-3 flex flex-col gap-2">
+                <p className="text-lg">{item.name}</p>
+                <p className="text-sm opacity-50">R$ {item.price.toFixed(2)}</p>
+                <Button
+                    variant="outline"
+                    onClick={handleAddButton}
+                >Adicionar</Button>
+            </div>
+        </div>
+    );
+}
--- exercicios/exc12/src/components/products/tab.tsx
+++ exercicios/exc12/src/components/products/tab.tsx
@@ -1,27 +1,65 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { getAllProducts } from "@/services/product";
+import { Product } from "@/types/product";
+import { ProductEmpty } from "./empty";
+import { ProductItem } from "./item";
+
+type Tab = {
+    title: string;
+    value: string;
+    products: Product[];
+}
 
 export const ProductsTab = async () => {
     const products = await getAllProducts();
 
+    const tabs: Tab[] = [
+        {
+            title: 'Sushi',
+            value: 'sushi',
+            products: products.filter(product => product.category === 'sushi')
+        },
+        {
+            title: 'Temaki',
+            value: 'temaki',
+            products: products.filter(product => product.category === 'temaki')
+        },
+        {
+            title: 'Combinados',
+            value: 'pack',
+            products: products.filter(product => product.category === 'pack')
+        },
+        {
+            title: 'Bebidas',
+            value: 'beverage',
+            products: products.filter(product => product.category === 'beverage')
+        }
+    ];
+
     return (
-        <Tabs defaultValue="tab1">
+        <Tabs defaultValue="sushi">
             <TabsList className="flex">
-                <TabsTrigger
-                    value="tab1"
-                    className="flex-1"
-                >tab 1</TabsTrigger>
-                <TabsTrigger
-                    value="tab2"
-                    className="flex-1"
-                >tab 2</TabsTrigger>
+                {tabs.map(item => (
+                    <TabsTrigger
+                        key={item.value}
+                        value={item.value}
+                        className="flex-1"
+                    >{item.title}</TabsTrigger>
+                ))}
             </TabsList>
-            <TabsContent value="tab1" className="mt-6">
-                tab 1 content
-            </TabsContent>
-            <TabsContent value="tab2" className="mt-6">
-                tab 2 content
-            </TabsContent>
+                {tabs.map(item => (
+                    <TabsContent key={item.value} value={item.value} className="mt-6">
+                        {item.products.length > 0 &&
+                            <div className="grid gap-5 grid-cols-2 sm:frid-cols-3 md:grid-cols-4">
+                                {item.products.map(product => (
+                                    <ProductItem key={product.id} item={product}/>
+                                ))}
+                                   
+                            </div>
+                        }
+                        {item.products.length === 0 && <ProductEmpty />}
+                    </TabsContent>
+                ))}
         </Tabs>
     );
 }
